22b90e60934d445e9489da88bc2278e07d0344a8
liuwan950628 <1027968784@qq.com>
2018-10-10 22:41:48 +0800

    travel-7

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="header">
+      城市选择
+      <div class="iconfont header-back">&#xe624;</div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+<style lang="stylus" scoped>
+    .header
+        overflow hidden
+        height .86rem
+        line-height .86rem
+        text-align center
+        color #fff
+        background red
+        font-size .32rem
+        .header-back
+        
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+      <div class="recommend-title">周末去哪儿</div>
+      <ul>
+          <li class="item border-bottom" v-for="item of list" :key="item.id">           
+              <div class="img-wrapper">
+                <img :src="item.imgUrl" alt="" class="item-img"> 
+              </div>   
+                              
+              <div class="item-info">
+                  <p class="item-title">{{item.title}}</p>
+                  <p class="item-desc">{{item.desc}}</p>
+              </div>
+          </li>
+      </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeWeekend',
+  props: {
+      list: Array
+  }
+}
+</script>
+<style lang="stylus" scoped>
+    .recommend-title
+        line-height .8rem
+        background #eeeeee
+        text-indent .2rem
+    .img-wrapper
+        overflow hidden
+        height 0
+        padding-bottom 37.9%
+        .item-img
+            width 100%
+    .item-info
+        padding .1rem
+        min-width 0
+        .item-title
+            line-height .54rem
+            font-size .32rem   
+        .item-desc
+            line-height .4rem
+            color: #cccccc
+</style>
