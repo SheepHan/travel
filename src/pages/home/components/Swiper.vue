28d49b881dcb8e379bea6c573786e8d7539f4781
liuwan950628 <1027968784@qq.com>
2018-10-14 15:32:11 +0800

    index-swiper

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+    <!-- slides -->
+    <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img :src="item.imgUrl" class="swiper-img">
+    </swiper-slide>
+    <!-- Optional controls -->
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  props: {
+    list: Array
+  },
+  data () {
+    return {
+      swiperOption: {
+        notNextTick: true,
+        pagination: '.swiper-pagination',
+        // autoplay: 1000,
+        speed: 300,
+        loop: true
+      },
+      swiperList: [{
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/82/c94c9a77de17f902.jpg_750x200_a8419488.jpg'
+      },
+      {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/86/deb33800de224c02.jpg_750x200_74fb795e.jpg'
+      }]
+    }
+  },
+  computed: {
+    showSwiper () {
+      return this.list.length
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 31.25%
+    // background: #eee
+    .swiper-img
+      width: 100%
+</style>
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,14 +1,17 @@
 <template>
     <div class="home">
         <home-header></home-header>
+        <home-swiper></home-swiper>
     </div>
 </template>
 <script>
 import HomeHeader from './components/header'
+import HomeSwiper from './components/Swiper'
 export default {
   name: 'Home',
   components: {
-    HomeHeader
+    HomeHeader,
+    HomeSwiper
   }
 }
 </script>
